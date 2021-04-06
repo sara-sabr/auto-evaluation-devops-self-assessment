6ef6c8c70bd36aa6732effe3eb992d8d2795a674
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-04-06 16:31:09 -0400

    Run npm lint

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,10 @@ module.exports = {
   //THere are some known issues with multi-page apps
   //jand using HTML history.pushState routing
   /*publicPath: "./",*/
-  publicPath: process.env.NODE_ENV === "production" ? "/auto-evaluation-devops-self-assessment/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/auto-evaluation-devops-self-assessment/"
+      : "/",
 
   configureWebpack: {
     devtool: "source-map"
