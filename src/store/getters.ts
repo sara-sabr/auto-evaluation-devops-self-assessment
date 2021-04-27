eb6ded8e9337c2d7614a84ad032d4d3978a789b1
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-04-27 16:26:07 -0400

    Add returnSectionsByPrefix and getters docs

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -2,14 +2,30 @@ import { GetterTree } from "vuex";
 import { RootState } from "@/store/state";
 import isEmpty from "lodash.isempty";
 import { Section } from "@/store/state";
-import { SurveyModel } from "survey-vue";
+import { PageModel, SurveyModel } from "survey-vue";
 
 export type Getters = {
+  /**
+   * Checks whether the store has any user data saved.
+   * @returns Returns true if data is saved, false if not
+   */
   inProgress(state: RootState): boolean;
+  /**
+   * Returns all app sections by name
+   * @returns An array of strings containing the sections names, an empty array if there no sections are available.
+   * */
   returnSectionsNames(state: RootState): string[];
+  /**
+   * @param state The store state
+   * @param sectionName A string containining the section name to evaluate
+   * @returns A Section object if the sectionName is found, undefined if not found.
+   */
   returnSectionByName(
     state: RootState
   ): (sectionName: string) => Section | undefined;
+  returnSectionsByPrefix(
+    state: RootState
+  ): (surveyData: SurveyModel, prefix: string) => PageModel[];
   returnCurrentSection(state: RootState): Section | undefined;
   resultsDataSections(state: RootState): any;
   returnSurveyModel(state: RootState): SurveyModel | undefined;
@@ -36,6 +52,17 @@ export const getters: GetterTree<RootState, RootState> & Getters = {
       return state.sections.find(
         section => section.sectionName === sectionName
       );
+    };
+  },
+  returnSectionsByPrefix(state: RootState) {
+    return (surveyData: SurveyModel, prefix: string) => {
+      let sections: PageModel[] = [];
+      surveyData.pages.forEach(page => {
+        if (page.name.includes(prefix)) {
+          sections.push(page);
+        }
+      });
+      return sections;
     };
   },
   returnCurrentSection(state: RootState) {
