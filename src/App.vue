fad6e5dd0d6136624d28dda9c79af6ad0d30f761
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-04-30 13:52:45 -0400

    Move to component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,16 @@
 <template>
   <div id="app">
     <i18n path=""></i18n>
-    <div>
-      <b-modal
-        id="welcome-modal"
-        :title="$t('notice.newUserModalTitle')"
-        v-model="newUser"
-      >
-        <p>
-          {{ $t("notice.newUserModalBody")
-          }}<a :href="$t('notice.newUserModalURL')">{{
-            $t("notice.newUserModalURLText")
-          }}</a>
-        </p>
-        <h5>{{ $t("notice.localSaveWarningSummary") }}</h5>
-        <p>{{ $t("notice.localSaveWarningParagraph") }}</p>
-        <p>
-          <strong>{{ $t("notice.exception") }}</strong>
-        </p>
-        <p>{{ $t("notice.localSaveWarningParagraph2") }}</p>
-        <b-form-checkbox
-          id="checkbox-notice"
-          v-model="checkbox"
-          name="checkbox-notice"
-          value="true"
-          unchecked-value="true"
-          size="lg"
-        >
-          {{ $t("notice.hideNotice") }}
-        </b-form-checkbox>
-      </b-modal>
-    </div>
+    <BaseNotice />
     <router-view />
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import BaseNotice from "@/components/BaseNotice.vue";
 export default Vue.extend({
-  data() {
-    return { newUser: true, checkbox: "true" };
+  components: {
+    BaseNotice
   }
 });
 </script>
---
+++ src/components/BaseNotice.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-modal
+      id="welcome-modal"
+      :title="$t('notice.newUserModalTitle')"
+      v-model="welcomeNotice"
+    >
+      <p>
+        {{ $t("notice.newUserModalBody")
+        }}<a :href="$t('notice.newUserModalURL')">{{
+          $t("notice.newUserModalURLText")
+        }}</a>
+      </p>
+      <h5>{{ $t("notice.localSaveWarningSummary") }}</h5>
+      <p>{{ $t("notice.localSaveWarningParagraph") }}</p>
+      <p>
+        <strong>{{ $t("notice.exception") }}</strong>
+      </p>
+      <p>{{ $t("notice.localSaveWarningParagraph2") }}</p>
+      <b-form-checkbox
+        id="checkbox-notice"
+        v-model="checkbox"
+        name="checkbox-notice"
+        value="false"
+        unchecked-value="true"
+        size="lg"
+      >
+        {{ $t("notice.hideNotice") }}
+      </b-form-checkbox>
+    </b-modal>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "BaseNotice",
+  data() {
+    return { welcomeNotice: true, checkbox: "false" };
+  },
+  mounted() {
+    this.$data.welcomeNotice = true;
+  }
+});
+</script>
