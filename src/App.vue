cbad669c99b6466f8aa71e9fb978895f7d593c83
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-04-30 15:34:10 -0400

    Add button to display welcome message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <i18n path=""></i18n>
+    <b-button @click="displayWelcomeMessage()">{{
+      $t("notice.displayWelcome")
+    }}</b-button>
     <BaseNotice />
     <router-view />
   </div>
@@ -11,6 +14,11 @@ import BaseNotice from "@/components/BaseNotice.vue";
 export default Vue.extend({
   components: {
     BaseNotice
+  },
+  methods: {
+    displayWelcomeMessage() {
+      this.$bvModal.show("welcome-modal");
+    }
   },
   mounted() {
     if (this.$store.getters.returnDisplayWelcome === true) {
