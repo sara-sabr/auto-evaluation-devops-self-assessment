3c689452cca9c750cb0cad2bfab7f7924ffbfe73
Guillaume Charest <guillaume.charest@hrsdc-rhdcc.gc.ca>
2021-05-05 17:15:47 -0400

    Add app created

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,20 @@
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+import { ActionTypes } from "./store/actions";
+import { Model } from "survey-vue";
+import surveyJSON from "@/survey-enfr.json";
+export default Vue.extend({
+  name: "App",
+  data() {
+    return { Survey: new Model(surveyJSON) };
+  },
+  created() {
+    this.$store.dispatch(ActionTypes.GetLocalAppData, undefined);
+    this.$store.dispatch(ActionTypes.SetAppData, this.Survey);
+    this.$store.dispatch(ActionTypes.SetSections, this.Survey);
+  }
+});
+</script>
