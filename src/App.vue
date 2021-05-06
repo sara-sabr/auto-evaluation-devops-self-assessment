c133739dbff9a094bc8417221a735f0ab6c3383f
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-05-06 15:49:02 -0400

    Remove call for app setup

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
-import { ActionTypes } from "./store/actions";
 import { Model } from "survey-vue";
 import surveyJSON from "@/survey-enfr.json";
 import { Recommendations } from "./store/state";
@@ -13,15 +12,5 @@ import { Recommendations } from "./store/state";
 export default class App extends Vue {
   survey: Model = new Model(surveyJSON);
   recommendations: Recommendations = this.$store.getters.returnRecommendations;
-
-  created() {
-    this.$store.dispatch(ActionTypes.GetLocalAppData, undefined);
-  }
-  mounted() {
-    if (this.$store.getters.inProgress === false) {
-      this.$store.dispatch(ActionTypes.SetAppData, this.survey);
-      this.$store.dispatch(ActionTypes.SetSections, this.survey);
-    }
-  }
 }
 </script>
